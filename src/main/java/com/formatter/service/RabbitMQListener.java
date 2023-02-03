9d326539e35af4e06d803d6c920142ce36685dcc
Sam <hidden@hidden.com>
2023-02-03 15:38:58 +0000

    parse json event

--- src/main/java/com/formatter/service/RabbitMQListener.java
+++ src/main/java/com/formatter/service/RabbitMQListener.java
@@ -1,6 +1,8 @@
 package com.formatter.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,6 +10,22 @@ import org.springframework.stereotype.Service;
 public class RabbitMQListener {
 
     public void consumeMessage(byte[] message) {
-        log.info("Consumed Message: " + message);
+        log.info("Consumed Message: " + new String(message));
+
+        // Process each unpacked file from each record
+        JSONObject obj = new JSONObject(new String(message));
+        JSONArray records = obj.getJSONArray("Records");
+        for (int i = 0; i < records.length(); i++) {
+            JSONObject record = records.getJSONObject(i);
+            JSONArray unpackedFiles = record.getJSONArray("unpacked");
+            for (int q = 0; q < unpackedFiles.length(); q++) {
+                JSONObject unpackedFile = records.getJSONObject(q);
+                String id = unpackedFile.getString("id");
+                String key = unpackedFile.getString("key");
+                String bucket = unpackedFile.getString("bucket");
+                log.info("key: {} - bucket: {}", key, bucket);
+            }
+        }
+
     }
 }
