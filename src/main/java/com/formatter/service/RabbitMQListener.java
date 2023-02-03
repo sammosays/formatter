0c0a4f6cced88482992399e62b75ddb9d976cc42
Sam <hidden@hidden.com>
2023-02-03 14:48:07 +0000

    consume message

--- src/main/java/com/formatter/service/RabbitMQListener.java
+++ src/main/java/com/formatter/service/RabbitMQListener.java
@@ -1,15 +1,13 @@
 package com.formatter.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class RabbitMQListener {
 
-    Logger logger = LoggerFactory.getLogger(RabbitMQListener.class);
-
     public void consumeMessage(String messageBody) {
-        logger.info("Consumed Message: " + messageBody);
+        log.info("Consumed Message: " + messageBody);
     }
 }
