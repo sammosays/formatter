5e4f1f70d1aafe9b9e27fba3582327705d4404fb
Sam <hidden@hidden.com>
2023-02-03 13:53:28 +0000

    attempted fix

--- src/main/java/com/formatter/config/RabbitMQConfig.java
+++ src/main/java/com/formatter/config/RabbitMQConfig.java
@@ -6,59 +6,80 @@ import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 public class RabbitMQConfig {
 
-    @Value("${input.notification.rabbitmq.queue}")
-    String queueName;
+	public static final String QUEUE_NAME = "formatter-queue";
 
-    @Value("${spring.rabbitmq.username}")
-    String username;
-
-    @Value("${spring.rabbitmq.password}")
-	String password;
-
-    @Value("${spring.rabbitmq.host}")
-	String host;
-
-    @Bean
-    Queue queue() {
-        return new Queue(queueName, false);
-    }
-
-    // create MessageListenerContainer using default connection factory
-//    @Bean
-//    MessageListenerContainer messageListenerContainer(ConnectionFactory connectionFactory) {
-//        SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
-//        simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
-//        simpleMessageListenerContainer.setQueues(queue());
-//        simpleMessageListenerContainer.setMessageListener(new RabbitMQListener());
-//        return simpleMessageListenerContainer;
-//
-//    }
-
-    // create custom connection factory
 	@Bean
-	ConnectionFactory connectionFactory() {
-		CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(host);
-//		cachingConnectionFactory.setUsername(username);
-//		cachingConnectionFactory.setUsername(password);
-		return cachingConnectionFactory;
-	}
-
-    // create MessageListenerContainer using custom connection factory
-	@Bean
-	MessageListenerContainer messageListenerContainer() {
+	SimpleMessageListenerContainer simpleMessageListenerContainer(ConnectionFactory connectionFactory, MessageListenerAdapter messageListenerAdapter) {
 		SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
-		simpleMessageListenerContainer.setConnectionFactory(connectionFactory());
-		simpleMessageListenerContainer.setQueues(queue());
-		System.out.println("setting queue to " + queueName);
-		simpleMessageListenerContainer.setMessageListener(new RabbitMQListener());
+		simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
+		simpleMessageListenerContainer.setQueueNames(QUEUE_NAME);
+		simpleMessageListenerContainer.setMessageListener(messageListenerAdapter);
 		return simpleMessageListenerContainer;
 	}
+
+	@Bean
+	MessageListenerAdapter messageListenerAdapter(RabbitMQListener rabbitMQListener) {
+		return new MessageListenerAdapter(rabbitMQListener, "consumeMessage");
+	}
 }
+
+
+//@Configuration
+//public class RabbitMQConfig {
+//
+//    @Value("${input.notification.rabbitmq.queue}")
+//    String queueName;
+//
+//    @Value("${spring.rabbitmq.username}")
+//    String username;
+//
+//    @Value("${spring.rabbitmq.password}")
+//	String password;
+//
+//    @Value("${spring.rabbitmq.host}")
+//	String host;
+//
+//    @Bean
+//    Queue queue() {
+//        return new Queue(queueName, false);
+//    }
+//
+//    // create MessageListenerContainer using default connection factory
+////    @Bean
+////    MessageListenerContainer messageListenerContainer(ConnectionFactory connectionFactory) {
+////        SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
+////        simpleMessageListenerContainer.setConnectionFactory(connectionFactory);
+////        simpleMessageListenerContainer.setQueues(queue());
+////        simpleMessageListenerContainer.setMessageListener(new RabbitMQListener());
+////        return simpleMessageListenerContainer;
+////
+////    }
+//
+//    // create custom connection factory
+//	@Bean
+//	ConnectionFactory connectionFactory() {
+//		CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(host);
+////		cachingConnectionFactory.setUsername(username);
+////		cachingConnectionFactory.setUsername(password);
+//		return cachingConnectionFactory;
+//	}
+//
+//    // create MessageListenerContainer using custom connection factory
+//	@Bean
+//	MessageListenerContainer messageListenerContainer() {
+//		SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
+//		simpleMessageListenerContainer.setConnectionFactory(connectionFactory());
+//		simpleMessageListenerContainer.setQueues(queue());
+//		System.out.println("setting queue to " + queueName);
+//		simpleMessageListenerContainer.setMessageListener(new RabbitMQListener());
+//		return simpleMessageListenerContainer;
+//	}
+//}
--- src/main/java/com/formatter/service/RabbitMQListener.java
+++ src/main/java/com/formatter/service/RabbitMQListener.java
@@ -1,12 +1,22 @@
 package com.formatter.service;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageListener;
+//import org.springframework.amqp.core.Message;
+//import org.springframework.amqp.core.MessageListener;
+//import org.springframework.stereotype.Service;
+
 import org.springframework.stereotype.Service;
 
 @Service
-public class RabbitMQListener implements MessageListener {
+public class RabbitMQListener {
 
-    public void onMessage(Message message) {
-        System.out.println("Consuming Message - " + new String(message.getBody()));
+    public void consumeMessage(String messageBody) {
+        System.out.println("Consumed Message: " + messageBody);
     }
 }
+
+//@Service
+//public class RabbitMQListener implements MessageListener {
+//
+//    public void onMessage(Message message) {
+//        System.out.println("Consuming Message - " + new String(message.getBody()));
+//    }
+//}
