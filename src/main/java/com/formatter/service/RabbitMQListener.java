e754bc431ca32496f3b06891cc961531dddfdf2b
Sam <hidden@hidden.com>
2023-03-09 19:34:01 +0000

    download from s3

--- src/main/java/com/formatter/service/RabbitMQListener.java
+++ src/main/java/com/formatter/service/RabbitMQListener.java
@@ -1,13 +1,23 @@
 package com.formatter.service;
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.util.IOUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 @Service
 @Slf4j
 public class RabbitMQListener {
+
+    private final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
 
     public void consumeMessage(String message) {
         log.info("Consumed Message: " + message);
@@ -23,9 +33,24 @@ public class RabbitMQListener {
                 String id = unpackedFile.getString("id");
                 String key = unpackedFile.getString("key");
                 String bucket = unpackedFile.getString("bucket");
-                log.info("key: {} - bucket: {}", key, bucket);
-            }
-        }
+                log.info("found key: {} - bucket: {}", key, bucket);
 
+                // download file from s3
+                try {
+                    byte[] content = downloadFromS3(key, bucket);
+                    log.info("content: " + Arrays.toString(content));
+
+                } catch (IOException e) {
+                    System.err.println(String.format("error downloading s3 object - key: %s - bucket: %s - %s",
+                            key, bucket, e.getMessage()));
+                }
+            }
+
+        }
+    }
+
+    public byte[] downloadFromS3(String key, String bucket) throws IOException {
+        S3Object object = s3.getObject("bucket", "key");
+        return IOUtils.toByteArray(object.getObjectContent());
     }
 }
