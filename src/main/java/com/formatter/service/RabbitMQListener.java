856bfa07d8b2bd04c4ae47148405b829a9c30500
Sam <hidden@hidden.com>
2023-03-09 21:43:24 +0000

    download from s3

--- src/main/java/com/formatter/service/RabbitMQListener.java
+++ src/main/java/com/formatter/service/RabbitMQListener.java
@@ -9,12 +9,15 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.util.IOUtils;
+import io.minio.GetObjectArgs;
+import io.minio.MinioClient;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 
 @Service
@@ -47,12 +50,10 @@ public class RabbitMQListener {
 
                 try {
                     // download file from s3
-                    byte[] content = downloadFromS3(key, bucket);
+                    byte[] content = downloadFromMinio(key, bucket);
                     log.info("content: " + Arrays.toString(content));
 
-                } catch (AmazonServiceException e) {
-                    log.error("error processing s3 object - key: {} - bucket: {} - {}", key, bucket, e.getErrorMessage());
-                } catch (SdkClientException | IOException e) {
+                } catch (Exception e) {
                     log.error("error processing s3 object - key: {} - bucket: {} - {}", key, bucket, e.getMessage());
                 }
             }
@@ -60,8 +61,20 @@ public class RabbitMQListener {
         }
     }
 
-    public byte[] downloadFromS3(String key, String bucket) throws IOException {
-        S3Object object = s3.getObject(bucket, key);
-        return IOUtils.toByteArray(object.getObjectContent());
+    public byte[] downloadFromMinio(String key, String bucket) throws Exception {
+//        S3Object object = s3.getObject(bucket, key);
+//        return IOUtils.toByteArray(object.getObjectContent());
+
+        MinioClient minioClient =
+                MinioClient.builder()
+                        .endpoint(MINIO_ENDPOINT)
+                        .credentials("AzsPRcioJ8aCuunm", "Mx2VUYufVrYlSQbvijglKUHSQJ1gHFvX")
+                        .build();
+        InputStream obj = minioClient.getObject(GetObjectArgs.builder()
+                .bucket(bucket)
+                .object(key)
+                .build());
+
+        return IOUtils.toByteArray(obj);
     }
 }
